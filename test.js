d691ad6e1d0a41cfa41916ef6cce18cebea6ef53
Nemolite <g25092011@mail.ru>
2017-10-20 11:10:53 +0300

    stop

--- test.js
+++ test.js
@@ -2,10 +2,13 @@
 
 
 
-function stackWords(word) {
+function stackWords() {
    var numberOfCalls = [];
 
-   var w = word;
+   if (arguments.length!=0)
+   var w = arguments[0];
+
+
    return function(w) {
      
      numberOfCalls.push(w);
@@ -23,3 +26,11 @@ var word ="грач";
 fn(word); //2
 var word ="собака";
 fn(word); //3
+
+fn();
+myArray = fn();
+
+myArray = myArray.filter(function(x) {
+    return x !== undefined && x !== null; 
+});
+console.log(myArray);
